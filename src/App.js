95207c9a073bd0f5cf057fb2716b61bcb691c04f
roman-agentic <leaddigit@ubuntu-internet-comp.us-central1-a.c.applied-buckeye-247918.internal>
2021-06-23 05:30:54 +0000

    first commit test

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Customers from './Customers'
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+
+
+class App extends Component {
+  render() {
+    console.log("Host URL"+process.env.PUBLIC_URL);
+    return (
+
+      <Router basename={process.env.PUBLIC_URL}>
+        <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Steven's first dfinity blockchain site</h1>
+        </header>
+          <p>Just adding some dummy data to test the visuals
+          <Switch>
+                <Route exact path= "/" render={() => (
+                  <Redirect to="/customerlist"/>
+                )}/>
+                 <Route exact path='/customerlist' component={Customers} />
+          </Switch>
+      </div>
+    </Router>
+    );
+  }
+}
+
+export default App;
